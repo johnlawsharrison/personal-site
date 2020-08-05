f064b675275dd085310657704741846d1ff21b22
John Laws Harrison <johnlawsharrison@gmail.com>
2020-08-04 19:20:22 -0700

    Update project grid for smallest screens

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -59,7 +59,7 @@ export default {
 /* Grid layout */
 .projects-grid {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   gap: 1rem;
 }
 
